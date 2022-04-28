b53457d65f5fa1536c5b42b23804b7c7142a8aed
rayssalves <rayssa.a.vieira@outlook.com>
2022-04-28 17:29:39 +0200

    setting up index.js

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require("express");
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(router);
-
 app.listen(port, () => console.log(`App listening on port ${port}!`));
-
-process.env.PORT || 4000;
